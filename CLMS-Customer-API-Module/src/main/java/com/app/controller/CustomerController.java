7939ba3f7b8151cd2093c317dbcdb04ba71b799a
shubhamkrmahto <skm9258@gmail.com>
2025-05-21 15:10:31 +0530

    Added Swagger

--- CLMS-Customer-API-Module/src/main/java/com/app/controller/CustomerController.java
+++ CLMS-Customer-API-Module/src/main/java/com/app/controller/CustomerController.java
@@ -2,8 +2,11 @@ package com.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,13 +22,13 @@ public class CustomerController {
 	@Autowired
 	private CustomerService cs;
 	
-	@RequestMapping("/savecustomer")
+	@PostMapping(value="/savecustomer", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public ResponseEntity<String> saveCustomer(@RequestPart("cJson") String cData , @RequestPart("profileImage") MultipartFile profileImage){
 		
 		return new ResponseEntity<String>(cs.saveCustomer(cData, profileImage) , HttpStatus.OK);
 	}
 	
-	@RequestMapping("/getcustomer/{un}/{pass}")
+	@GetMapping("/getcustomer/{un}/{pass}")
 	public ResponseEntity<Customer> getCustomer(@PathVariable("un") String un , @PathVariable("pass") String pass){
 		Customer allCustomerInfo = cs.getAllCustomerInfo(un, pass);
 		return new ResponseEntity<Customer>(allCustomerInfo , HttpStatus.OK);
